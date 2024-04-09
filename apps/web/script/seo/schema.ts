1678c0b7c0c3abb53fb60dc9012f6cdf9f0e048b
GervinFung <gervinfungdaxuen@gmail.com>
2024-04-09 15:05:55 +0800

    fix(apps/web/schema): create directory if not created before writing schema

--- apps/web/script/seo/schema.ts
+++ apps/web/script/seo/schema.ts
@@ -5,6 +5,10 @@ import { generatePaths } from '../../test/snapshot/data';
 const main = () => {
 	const paths = generatePaths();
 
+	if (!fs.existsSync('src/web/generated')) {
+		fs.mkdirSync('src/web/generated');
+	}
+
 	fs.writeFileSync(
 		'src/web/generated/schema.ts',
 		`const paths = ${JSON.stringify(paths, undefined, 4)}; export default paths;`
