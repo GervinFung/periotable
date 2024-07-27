d8bdc94a771879a72ce6cbedb02cb4dafc4d23c0
GervinFung <gervinfungdaxuen@gmail.com>
2024-07-27 12:15:43 +0800

    fix(data): improve data processing

--- packages/data/script/data.ts
+++ packages/data/script/data.ts
@@ -6,37 +6,42 @@ import dotenv from 'dotenv';
 
 import axios from 'axios';
 
-const asString = <Str extends string>(string: Str | undefined) => {
+const asString = <Str extends string>(string: Str | undefined | null) => {
 	if (string) {
 		return string;
 	}
 
-	throw new Error('');
+	throw new Error(`Expected a string, but got "${string}"`);
 };
 
 const main = async () => {
 	dotenv.config();
 
 	const octokit = new Octokit({
-		auth: process.env.TOKEN,
+		auth: process.env['TOKEN'],
 	});
 
 	const result = await octokit.rest.repos.getContent({
-		owner: asString(process.env.OWNER),
-		repo: asString(process.env.REPO),
-		path: asString(process.env.DATA_PATH),
+		owner: asString(process.env['OWNER']),
+		repo: asString(process.env['REPO']),
+		path: asString(process.env['DATA_PATH']),
 	});
 
-	// @ts-expect-error: Download URL exists because data is of type "file" but it doesn't have type
-	const url = result.data.download_url as string;
+	const { data } = result;
 
-	const content = await axios.get(url).then((response) => {
-		if (typeof response.data !== 'string') {
-			throw new Error('Data is not a string');
-		}
+	if (Array.isArray(data)) {
+		throw new Error('Data is an array');
+	}
 
-		return response.data;
-	});
+	const content = await axios
+		.get(asString(data.download_url))
+		.then((response) => {
+			if (typeof response.data !== 'string') {
+				throw new Error('Data is not a string');
+			}
+
+			return response.data;
+		});
 
 	fs.writeFileSync('index.ts', content);
 };
